42cfc90ede9ec3161d4af71fc52b97e19e1da3c4
Anonim Education <soraplay111@gmail.com>
2021-08-15 12:24:17 +0300

    Добавьте файлы проекта.

---
+++ Урок 5 шарп/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Урок_5_шарп
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1) while
+            // 2) do while
+            // 3) for
+            // 4) foreach
+
+
+            int num = 0;
+
+            /*for (int i = 0; i < 5; i++)
+            {
+                if (true)
+                {
+                    Console.WriteLine("Какой-то текст");
+                }
+            }*/
+
+            if (true)
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    Console.WriteLine("Какой-то текст");
+                }
+            }
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
